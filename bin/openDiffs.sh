97e7aeff95cff7eb3e0fc65a48967daa0f5e94da
Paige Park <ppark@qualtrics.com>
2018-01-23 15:21:59 -0800

    bash script to open all diffs:

--- bin/openDiffs.sh
+++ bin/openDiffs.sh
@@ -1,1 +1,7 @@
-vim $(git diff $1 | egrep "^....b/" | cut -d " " -f 2 | cut -d "/" -f 2-)
+#!/bin/bash
+editor="vim"
+if [ "$1" != "" ]; then
+	$editor $(git diff "$1" | egrep "^....b/" | cut -d " " -f 2 | cut -d "/" -f 2-)
+else
+	echo "Usage: openDiffs <branch>"
+fi
